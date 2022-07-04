10885abd1ca5e1fa0aa9c43eb18948d20070bb92
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-07-04 07:17:23 -0600

    Added new issue with traits and self types

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -2,6 +2,7 @@ class Test {
 
   inline def test(fun: => Any): Any = fun
 
+  /* This works as of 3.1.3 */
   test {
     case class Pair[X, Y](
       x: X,
@@ -9,4 +10,27 @@ class Test {
     )
   }
 
+    /**
+     * This does not work as of 3.1.3
+     * Self-types are not resolved, this does work though if
+     * Moveable and Vehicle are removed from the test context
+     */
+  test {
+     transparent trait Moveable {
+         def increaseSpeed(ms: Int): Moveable
+         def decreaseSpeed(ms: Int): Moveable
+     }
+
+     trait Vehicle { self: Moveable =>
+         def make: String
+         def model: String
+     }
+
+     class Car(val make: String, val model: String, val currentSpeed: Int) extends Vehicle with Moveable {
+         override def increaseSpeed(ms: Int):Car = new Car(make, model, currentSpeed + ms)
+         override def decreaseSpeed(ms: Int):Car = new Car(make, model, currentSpeed - ms)
+     }
+
+     val ford:Car = new Car("Ford", "Fiesta", 110).decreaseSpeed(20)
+  }
 }
